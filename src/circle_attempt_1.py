3a3d9abd9935ea5931250c202e383446e7db9028
Alekhya <54409677+alekhyaramarao@users.noreply.github.com>
2019-08-27 10:18:49 +0530

    Add files via upload

--- src/circle_attempt_1.py
+++ src/circle_attempt_1.py
@@ -2,27 +2,23 @@ import turtle
 wn= turtle.Screen()
 wn.bgcolor("pink")
 
-circ=turtle.Turtle()
+def create_cir(radius,x,y,color):
+    circ=turtle.Turtle()
+    circ.up()
+    circ.speed(0)
+    circ.shape("classic")
+    circ.goto(x,y)
+    for _ in range(36):
+        circ.color(color)
+        circ.forward(radius)
+        circ.stamp()
+        circ.forward(-radius)
+        circ.left(10)
+    
+           
 
-circ.up()
-circ.shape("classic")
-circ.speed(0)
-circ.goto(-50,0)
-for _ in range (36):
-    circ.color("#CD5C5C")
-    circ.stamp()
-    circ.forward(50)  
-    circ.left(10)
-    circ.forward(-50)
-    
-circ.goto(-75,0)
-for _ in range (36):
-    circ.color("blue")
-    circ.stamp()
-    circ.forward(75)  
-    circ.left(10)
-    circ.forward(-75)
-    
+create_cir(50,0,0,"blue")
+create_cir(75,0,0,"yellow")
 
     
     
